c47d2ebdc6557c3716537b7e1d5036b68dc8bd98
Yuvaraj Dekhane <148527838+yuvaraj8080@users.noreply.github.com>
2025-01-22 23:33:15 +0530

    searchin and management ledger

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2460775227"
+    serviceWorkerVersion: "2887995384"
   }
 });
